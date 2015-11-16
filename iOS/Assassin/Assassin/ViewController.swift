873c01ea44aec579113b22ca75392cfccf0cdced
Yuanhao Li <yuanhao.li@gmail.com>
2015-11-16 16:55:52 +0100

    We can connect to the server now. (Socket.io)

--- iOS/Assassin/Assassin/ViewController.swift
+++ iOS/Assassin/Assassin/ViewController.swift
@@ -7,22 +7,35 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class VictimViewController: UIViewController {
 
     @IBOutlet weak var victimStatusImage: UIImageView!
     @IBOutlet weak var victimHPLabel: UILabel!
     
+    var socket: SocketIOClient!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.setupSocketIO()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func setupSocketIO() {
+        self.socket = SocketIOClient(socketURL: "127.0.0.1:3001", options: [.Log(true), .ForcePolling(true)])
+        self.socket.on("connect", callback: { data, ack in
+            print(data)
+            print(ack)
+            print(self.socket.sid)
+        })
+        
+        self.socket.connect()
+    }
 }
 
