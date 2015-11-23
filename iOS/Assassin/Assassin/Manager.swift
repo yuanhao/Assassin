e8a9c7b632097103b8a031096106d1c4c5032292
Yuanhao Li <yuanhao.li@gmail.com>
2015-11-23 11:46:51 +0100

    image rename

---
+++ iOS/Assassin/Assassin/Manager.swift
@@ -0,0 +1,17 @@
+//
+//  Delegate.swift
+//  Assassin
+//
+//  Created by Yuanhao Li on 22/11/15.
+//  Copyright © 2015 Yuanhao Li. All rights reserved.
+//
+
+import Foundation
+
+
+class VictimSocketManagerDelegate: SocketManagerDelegate {
+    
+    func socket(socket: SocketIOClient, onConnect data: AnyObject, ack: SocketAckEmitter?) {
+        <#code#>
+    }
+}
---
+++ iOS/Assassin/Assassin/SocketIOManager.swift
@@ -0,0 +1,13 @@
+//
+//  SocketIOManager.swift
+//  Assassin
+//
+//  Created by Yuanhao Li on 22/11/15.
+//  Copyright © 2015 Yuanhao Li. All rights reserved.
+//
+
+import UIKit
+
+class SocketIOManager: NSObject {
+
+}
